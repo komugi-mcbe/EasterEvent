050bcd6d1ff085651f7ad3c8fe0972c8e2080612
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-05-08 13:48:04 +0900

    Add files via upload

---
+++ src/xtakumatutix/easter/Main.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace xtakumatutix\easter;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\level\Level;
+use pocketmine\Player;
+use pocketmine\item\Item;
+
+Class Main extends PluginBase implements Listener {
+
+    public function onEnable(){
+        $this->getLogger()->notice("読み込み完了_ver.1.0.0");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+	public function onBreak(BlockBreakEvent $event)
+	{
+		$player = $event->getPlayer();
+		$block = $event->getBlock()->getID();
+		switch($player->getlevel()->getName())
+		{
+			case "event";
+			if($block == 17){
+				if(mt_rand(0 , 1) == 1){
+					$this->woodegg($player);
+				}
+			}
+			
+			if($block == 2 or $block == 3){
+				if(mt_rand(0 , 1) == 1){
+					$this->soilegg($player);
+				}
+			}
+			break;
+		}
+	}
+
+    public function woodegg(Player $player)
+    {
+    	if(!$player->getInventory()->canAddItem(Item::get(344,0,1))){
+    		$player->sendMessage(" >> おっと卵をゲットしたのにインベントリがいっぱいで、捨てちゃった...");
+    	}else{
+    		$item = Item::get(344,0,1);
+    		$item->setCustomName("§6木§fの§e卵§r");
+    		$player->getInventory()->addItem($item);
+    		$player->sendMessage("§a >> §6木§fの§e卵§fをゲット！");
+    	}
+    }
+
+    public function soilegg(Player $player)
+    {
+    	if(!$player->getInventory()->canAddItem(Item::get(344,0,1))){
+    		$player->sendMessage(" >> おっと卵をゲットしたのにインベントリがいっぱいで、捨てちゃった...");
+    	}else{
+    		$item = Item::get(344,0,1);
+    		$item->setCustomName("§6土§fの§e卵§r");
+    		$player->getInventory()->addItem($item);
+    		$player->sendMessage("§a >> §6土§fの§e卵§fをゲット！");
+    	}
+    }
+}
