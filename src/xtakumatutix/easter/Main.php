a82a33a9488af42535575c650a97cc6fe95eaaef
yurisi0212 <Dev.yurisi@gmail.com>
2020-05-08 15:12:01 +0900

    コードの整形

--- src/xtakumatutix/easter/Main.php
+++ src/xtakumatutix/easter/Main.php
@@ -5,96 +5,53 @@ namespace xtakumatutix\easter;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\level\Level;
 use pocketmine\Player;
 use pocketmine\item\Item;
 
 Class Main extends PluginBase implements Listener {
 
-    public function onEnable(){
+    public function onEnable() {
         $this->getLogger()->notice("読み込み完了_ver.1.0.0");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-	public function onBreak(BlockBreakEvent $event)
-	{
-		$player = $event->getPlayer();
-		$block = $event->getBlock()->getID();
-		switch($player->getlevel()->getName())
-		{
-			case "event";
-			if($block == 17){
-				if(mt_rand(0 , 1) == 1){
-					$this->woodegg($player);
-				}
-			}
+    public function onBreak(BlockBreakEvent $event) {
+        $player = $event->getPlayer();
+        $block = $event->getBlock()->getID();
 
-			if($block == 2 or $block == 3){
-				if(mt_rand(0 , 1) == 1){
-					$this->soilegg($player);
-				}
-			}
+        if ($player->getLevel()->getName() == "event") {
 
-			if($block == 1){
-				if(mt_rand(0 , 1) == 1){
-				    $this->stoneegg($player);
-				}
-			}
+            if ($block === 17 and mt_rand(0, 1) === 1) {
+                self::addegg($player, "§6木§fの§e卵§r");
+            }
 
-			if($block == 18){
-				if(mt_rand(0 , 1) == 1){
-					$this->reefegg($player);
-				}
-			}
-			break;
-		}
-	}
+            if ($block === 2 or $block === 3) {
+                if (mt_rand(0, 1) === 1) {
+                    self::addegg($player, "§6土§fの§e卵§r");
+                }
+            }
 
-    public function woodegg(Player $player)
-    {
-    	if(!$player->getInventory()->canAddItem(Item::get(344,0,1))){
-    		$player->sendMessage(" >> おっと卵をゲットしたのにインベントリがいっぱいで、捨てちゃった...");
-    	}else{
-    		$item = Item::get(344,0,1);
-    		$item->setCustomName("§6木§fの§e卵§r");
-    		$player->getInventory()->addItem($item);
-    		$player->sendMessage("§a >> §6木§fの§e卵§fをゲット！");
-    	}
+            if ($block == 1 and mt_rand(0, 1) === 1) {
+                self::addegg($player, "§7石§fの§e卵§r");
+
+            }
+
+            if ($block === 18 and mt_rand(0, 1) === 1) {
+                self::addegg($player, "§a葉っぱ§fの§e卵§r");
+
+            }
+        }
     }
 
-    public function soilegg(Player $player)
-    {
-    	if(!$player->getInventory()->canAddItem(Item::get(344,0,1))){
-    		$player->sendMessage(" >> おっと卵をゲットしたのにインベントリがいっぱいで、捨てちゃった...");
-    	}else{
-    		$item = Item::get(344,0,1);
-    		$item->setCustomName("§6土§fの§e卵§r");
-    		$player->getInventory()->addItem($item);
-    		$player->sendMessage("§a >> §6土§fの§e卵§fをゲット！");
-    	}
+    public static function addegg(Player $player, string $eggname) {
+        if (!$player->getInventory()->canAddItem(Item::get(344, 0, 1))) {
+            $player->sendMessage(" >> おっと卵をゲットしたのにインベントリがいっぱいで、捨てちゃった...");
+            return;
+        }
+        $item = Item::get(344, 0, 1);
+        $item->setCustomName($eggname);
+        $player->getInventory()->addItem($item);
+        $player->sendMessage("§a >> {$eggname}をゲット！");
     }
 
-    public function stoneegg(Player $player)
-    {
-    	if(!$player->getInventory()->canAddItem(Item::get(344,0,1))){
-    		$player->sendMessage(" >> おっと卵をゲットしたのにインベントリがいっぱいで、捨てちゃった...");
-    	}else{
-    		$item = Item::get(344,0,1);
-    		$item->setCustomName("§7石§fの§e卵§r");
-    		$player->getInventory()->addItem($item);
-    		$player->sendMessage("§a >> §7石§fの§e卵§fをゲット！");
-    	}
-    }
-
-    public function reefegg(Player $player)
-    {
-    	if(!$player->getInventory()->canAddItem(Item::get(344,0,1))){
-    		$player->sendMessage(" >> おっと卵をゲットしたのにインベントリがいっぱいで、捨てちゃった...");
-    	}else{
-    		$item = Item::get(344,0,1);
-    		$item->setCustomName("§a葉っぱ§fの§e卵§r");
-    		$player->getInventory()->addItem($item);
-    		$player->sendMessage("§a >> §a葉っぱ§fの§e卵§fをゲット！");
-    	}
-    }
 }
